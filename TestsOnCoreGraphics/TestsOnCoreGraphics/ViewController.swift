1d1d87361a5442cb410215a53877b5f91e47f902
GIWON1 <giwon.seo@nexstreaming.com>
2018-03-12 11:28:40 +0900

    added a testcode project for core graphics

---
+++ TestsOnCoreGraphics/TestsOnCoreGraphics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestsOnCoreGraphics
+//
+//  Created by GIWON1 on 2018. 3. 12..
+//  Copyright © 2018년 GIWON1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
