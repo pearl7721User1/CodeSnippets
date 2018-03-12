0ac299948a3f4606d9d5a4b82866bf53c9aa5be4
GIWON1 <giwon.seo@nexstreaming.com>
2018-03-12 10:46:15 +0900

    added testbed project for concurrency

---
+++ Concurrency/TestBedForConcurrency/TestBedForConcurrency/ConcurrentOperations/AlertOperation.swift
@@ -0,0 +1,109 @@
+/*
+ Copyright (C) 2015 Apple Inc. All Rights Reserved.
+ See LICENSE.txt for this sample’s licensing information
+ 
+ Abstract:
+ This file shows how to present an alert as part of an operation.
+ */
+
+import UIKit
+
+class AlertOperation: Operation {
+    // MARK: Properties
+    
+    private let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .alert)
+    private let presentationContext: UIViewController? = nil
+    
+    // MARK: CunCurrency
+    private var _executing = false {
+        willSet {
+            willChangeValue(forKey: "isExecuting")
+        }
+        didSet {
+            didChangeValue(forKey: "isExecuting")
+        }
+    }
+    
+    override var isExecuting: Bool {
+        return _executing
+    }
+    
+    private var _finished = false {
+        willSet {
+            willChangeValue(forKey: "isFinished")
+        }
+        
+        didSet {
+            didChangeValue(forKey: "isFinished")
+        }
+    }
+    
+    override var isFinished: Bool {
+        return _finished
+    }
+    
+    func executing(_ executing: Bool) {
+        _executing = executing
+    }
+    
+    func finish(_ finished: Bool) {
+        _finished = finished
+    }
+    
+    
+    
+    var title: String? {
+        get {
+            return alertController.title
+        }
+        
+        set {
+            alertController.title = newValue
+            name = newValue
+        }
+    }
+    
+    var message: String? {
+        get {
+            return alertController.message
+        }
+        
+        set {
+            alertController.message = newValue
+        }
+    }
+    
+    
+    func addAction(title: String, style: UIAlertActionStyle = .default, handler: @escaping (AlertOperation) -> Void = { _ in }) {
+        let action = UIAlertAction(title: title, style: style) { [weak self] _ in
+            if let strongSelf = self {
+                handler(strongSelf)
+            }
+            
+            self?.executing(false)
+            self?.finish(true)
+        }
+        
+        alertController.addAction(action)
+    }
+    
+    override func start() {
+        
+        executing(true)
+        
+        DispatchQueue.main.async {
+            if self.alertController.actions.isEmpty {
+                self.addAction(title: "OK")
+            }
+            
+            if let appDelegate = UIApplication.shared.delegate,
+                let appWindow = appDelegate.window!,
+                let rootViewController = appWindow.rootViewController {
+                rootViewController.present(self.alertController, animated: true, completion: nil)
+            }
+            
+        }
+        
+    }
+}
+
---
+++ Concurrency/TestBedForConcurrency/TestBedForConcurrency/OperationTestViewController.swift
@@ -0,0 +1,52 @@
+//
+//  OperationTestViewController.swift
+//  TestBedForConcurrency
+//
+//  Created by GIWON1 on 2018. 3. 9..
+//  Copyright © 2018년 GIWON1. All rights reserved.
+//
+
+import UIKit
+
+class OperationTestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        
+        
+        let operation1 = AlertOperation()
+        let operation2 = AlertOperation()
+        let operation3 = AlertOperation()
+        
+        //        operation3.addDependency(operation2)
+        //        operation2.addDependency(operation1)
+        
+        
+        let operationQueue = OperationQueue()
+        operationQueue.maxConcurrentOperationCount = 1
+        operationQueue.addOperation(operation1)
+        operationQueue.addOperation(operation2)
+        operationQueue.addOperation(operation3)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
